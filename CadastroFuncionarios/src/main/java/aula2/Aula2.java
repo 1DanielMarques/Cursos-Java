4c77043609d99f36582ef06fad9b01fe1f030222
mDanielSE <daniel.marques@edu.unifil.br>
2022-08-04 16:50:27 -0300

    primeiros passos

---
+++ CadastroFuncionarios/src/main/java/aula2/Aula2.java
@@ -0,0 +1,16 @@
+package aula2;
+
+/**
+ * @author mDanielSE
+ */
+public class Aula2 {
+
+    public static void main(String[] args) {
+
+        Cadeira c1 = new Cadeira();
+        c1.serrar();
+        c1.lixar();
+        c1.pintar();
+
+    }
+}
