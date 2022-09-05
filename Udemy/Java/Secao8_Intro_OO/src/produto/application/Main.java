618fb921e73925be45e18b8ba20e9367e0ca91f9
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-05 14:29:14 -0300

    implementando laço p/ inserir novamente

--- Udemy/Java/Secao8_Intro_OO/src/produto/application/Main.java
+++ Udemy/Java/Secao8_Intro_OO/src/produto/application/Main.java
@@ -9,16 +9,24 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        String opcao = "";
         Produto produto = new Produto();
-        System.out.println("Dados do produto:");
-        System.out.print("Nome: ");
-        produto.nome = sc.nextLine();
-        System.out.print("Preço: ");
-        produto.preco = sc.nextDouble();
-        System.out.print("Quantidade: ");
-        produto.quantidade = sc.nextInt();
+        do {
+            System.out.println("Dados do produto:");
+            System.out.print("Nome: ");
+            produto.nome = sc.nextLine();
+            System.out.print("Preço: ");
+            produto.preco = sc.nextDouble();
+            System.out.print("Quantidade: ");
+            produto.quantidade = sc.nextInt();
+            System.out.println(produto.nome + ", " + String.format("%.2f", produto.preco) + ", " + produto.quantidade);
+            System.out.println("Deseja alterar algum dado? S/N");
+            opcao = sc.next();
+            sc.nextLine();
+            opcao = opcao.toLowerCase();
+        } while (!opcao.equals("n"));
 
-        System.out.println(produto.nome+", "+String.format("%.2f",produto.preco)+", "+produto.quantidade);
+
 
 
         sc.close();
