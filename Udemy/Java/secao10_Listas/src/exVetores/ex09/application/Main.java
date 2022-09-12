88fb03019d63a472c45e29853afc7fdb0cbc9968
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-12 15:43:52 -0300

    exFixacao_vetores_alunosAprovados

---
+++ Udemy/Java/secao10_Listas/src/exVetores/ex09/application/Main.java
@@ -0,0 +1,35 @@
+package exVetores.ex09.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Quantas pessoas voce vai digitar? ");
+        int n = sc.nextInt();
+        String vetNome[] = new String[n];
+        int vetIdade[] = new int[n];
+        int maiorIdade = 0, indiceMaior = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.println("Dados da " + (i + 1) + "a pessoa:");
+            System.out.print("Nome: ");
+            sc.nextLine();
+            vetNome[i] = sc.nextLine();
+            System.out.print("Idade: ");
+            vetIdade[i] = sc.nextInt();
+            if (i == 0) {
+                maiorIdade = vetIdade[0];
+            }
+            if (vetIdade[i] > maiorIdade) {
+                maiorIdade = vetIdade[i];
+                indiceMaior = i;
+            }
+        }
+        System.out.println("PESSOA MAIS VELHA: " + vetNome[indiceMaior]);
+
+
+        sc.close();
+    }
+}
--- Udemy/Java/secao10_Listas/src/exVetores/ex10/application/Main.java
+++ Udemy/Java/secao10_Listas/src/exVetores/ex10/application/Main.java
@@ -1,5 +1,7 @@
 package exVetores.ex10.application;
 
+import exVetores.ex10.entities.Aluno;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,29 +9,30 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        System.out.print("Quantas pessoas voce vai digitar? ");
+        String nome;
+        double n1, n2;
+        System.out.print("Quantos alunos serao digitados? ");
         int n = sc.nextInt();
-        String vetNome[] = new String[n];
-        int vetIdade[] = new int[n];
-        int maiorIdade = 0, indiceMaior = 0;
-        for (int i = 0; i < n; i++) {
-            System.out.println("Dados da " + (i + 1) + "a pessoa:");
-            System.out.print("Nome: ");
+        Aluno vet[] = new Aluno[n];
+        for (int i = 0; i < vet.length; i++) {
+            System.out.println("Digite nome, primeira e segunda nota do " + (i + 1) + "o aluno:");
             sc.nextLine();
-            vetNome[i] = sc.nextLine();
-            System.out.print("Idade: ");
-            vetIdade[i] = sc.nextInt();
-            if (i == 0) {
-                maiorIdade = vetIdade[0];
-            }
-            if (vetIdade[i] > maiorIdade) {
-                maiorIdade = vetIdade[i];
-                indiceMaior = i;
-            }
+            nome = sc.nextLine();
+            n1 = sc.nextDouble();
+            n2 = sc.nextDouble();
+            vet[i] = new Aluno(nome,n1,n2);
         }
-        System.out.println("PESSOA MAIS VELHA: " + vetNome[indiceMaior]);
+        System.out.println("Alunos aprovados:");
+        for (int i = 0; i < vet.length; i++) {
+            if (vet[i].media()) {
+                System.out.println(vet[i].getNome());
+            }
+
+        }
 
 
         sc.close();
+
     }
 }
+
