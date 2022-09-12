c6c635e67dc5348a9d3832d81bbb9b9c91a6a076
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-12 15:11:30 -0300

    exFixacao_vetores_abaixoMedia

---
+++ Udemy/Java/secao10_Listas/src/exVetores/ex07/application/Main.java
@@ -0,0 +1,31 @@
+package exVetores.ex07.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Quantos elementos vai ter o vetor? ");
+        int n = sc.nextInt();
+        double vet[] = new double[n];
+        double soma = 0;
+        for (int i = 0; i < vet.length; i++) {
+            System.out.print("Digite um numero: ");
+            vet[i] = sc.nextDouble();
+            soma += vet[i];
+        }
+        double media = soma / n;
+        System.out.printf("MEDIA DO VETOR = %.3f%n", media);
+        System.out.println("ELEMENTOS ABAIXO DA MEDIA:");
+        for (int i = 0; i < vet.length; i++) {
+            if (vet[i] < media) {
+                System.out.println(vet[i]);
+            }
+        }
+
+        sc.close();
+
+    }
+}
