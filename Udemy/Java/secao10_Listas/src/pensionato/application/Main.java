debecccd02c08643f108e0f930ab71e6582dae89
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-13 16:38:04 -0300

    desafio Pensionato

---
+++ Udemy/Java/secao10_Listas/src/pensionato/application/Main.java
@@ -0,0 +1,41 @@
+package pensionato.application;
+
+import pensionato.entities.Estudante;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Estudante quartos[] = new Estudante[10];
+        String nome, email;
+        int quarto;
+
+        System.out.print("Quantos quartos serão alugados? ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Aluguel #" + (i + 1) + ":");
+            System.out.print("Nome: ");
+            sc.nextLine();
+            nome = sc.nextLine();
+            System.out.print("Email: ");
+            email = sc.nextLine();
+            System.out.print("Quarto: ");
+            quarto = sc.nextInt();
+            quartos[quarto] = new Estudante(nome, email, quarto);
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Quartos ocupados:");
+        for (int i = 0; i < quartos.length; i++) {
+            if (quartos[i] != null) {
+                System.out.println(quartos[i].toString());
+            }
+        }
+
+        sc.close();
+
+    }
+}
