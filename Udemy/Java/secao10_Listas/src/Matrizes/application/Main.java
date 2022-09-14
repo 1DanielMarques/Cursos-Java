fa48a1890bc623d60e2636184d2955c598992696
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-14 17:19:04 -0300

    Matrizes-diagonal principal e numeros negativos

--- Udemy/Java/secao10_Listas/src/Matrizes/application/Main.java
+++ Udemy/Java/secao10_Listas/src/Matrizes/application/Main.java
@@ -1,4 +1,43 @@
 package Matrizes.application;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Linhas e Colunas: ");
+        int n = sc.nextInt();
+        int[][] matriz = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("Linha " + (i + 1) + " Coluna " + (j + 1) + ": ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print("|");
+            for (int j = 0; j < n; j++) {
+                System.out.print(matriz[i][j] + " | ");
+
+            }
+            System.out.println();
+        }
+        System.out.println("Diagonal principal:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(matriz[i][i] + " ");
+        }
+        int contador = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matriz[i][j] < 0) {
+                    contador++;
+                }
+            }
+        }
+        System.out.println("\nNumeros negativos = " + contador);
+
+        sc.close();
+    }
+
 }
