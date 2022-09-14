fc55f6a443bb3348b5ad4f254d5ffcea0ff53a32
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-14 17:23:16 -0300

    Trocando n por matriz.length

--- Udemy/Java/secao10_Listas/src/Matrizes/application/Main.java
+++ Udemy/Java/secao10_Listas/src/Matrizes/application/Main.java
@@ -9,27 +9,27 @@ public class Main {
         int n = sc.nextInt();
         int[][] matriz = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
                 System.out.print("Linha " + (i + 1) + " Coluna " + (j + 1) + ": ");
                 matriz[i][j] = sc.nextInt();
             }
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < matriz.length; i++) {
             System.out.print("|");
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < matriz[i].length; j++) {
                 System.out.print(matriz[i][j] + " | ");
 
             }
             System.out.println();
         }
         System.out.println("Diagonal principal:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < matriz.length; i++) {
             System.out.print(matriz[i][i] + " ");
         }
         int contador = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
                 if (matriz[i][j] < 0) {
                     contador++;
                 }
