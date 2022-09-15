3a0df87441e477b721a58c2d2d6c3e394048ea4e
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-15 16:32:58 -0300

    Exercicio finalizado

--- Udemy/Java/secao10_Listas/src/Matrizes/posicaoNumeros/application/Main.java
+++ Udemy/Java/secao10_Listas/src/Matrizes/posicaoNumeros/application/Main.java
@@ -14,19 +14,40 @@ public class Main {
         System.out.println("Insira valores inteiros");
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
-                System.out.print("Linha "+(i+1)+" Coluna "+(j+1)+": ");
+                System.out.print("Linha " + (i + 1) + " Coluna " + (j + 1) + ": ");
                 mat[i][j] = sc.nextInt();
             }
         }
         for (int i = 0; i < mat.length; i++) {
             System.out.print("| ");
             for (int j = 0; j < mat[i].length; j++) {
-                System.out.print(mat[i][j]+" | ");
+                System.out.print(mat[i][j] + " | ");
             }
             System.out.println();
         }
         System.out.print("Informe um número da matriz: ");
         int numero = sc.nextInt();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] == numero) {
+                    System.out.println("Posicao: " + i + "," + j);
+                    if (j != 0) {
+                        System.out.println("Esquerda: " + mat[i][j - 1]);
+                    }
+                    if (j != mat[i].length - 1) {
+                        System.out.println("Direita: " + mat[i][j + 1]);
+                    }
+                    if (i != 0) {
+                        System.out.println("Cima: " + mat[i - 1][j]);
+                    }
+                    if (i != mat.length - 1) {
+                        System.out.println("Baixo: " + mat[i + 1][j]);
+                    }
+                    System.out.println("---------------------------");
+                }
+
+            }
+        }
 
 
         sc.close();
