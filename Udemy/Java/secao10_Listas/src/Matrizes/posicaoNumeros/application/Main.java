986a426ba8ac3b2d1d47605c25b5c20eb496da78
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-15 16:03:09 -0300

    Entrada de dados e impressao da matriz

--- Udemy/Java/secao10_Listas/src/Matrizes/posicaoNumeros/application/Main.java
+++ Udemy/Java/secao10_Listas/src/Matrizes/posicaoNumeros/application/Main.java
@@ -10,12 +10,23 @@ public class Main {
         System.out.print("Colunas: ");
         int n = sc.nextInt();
         int[][] mat = new int[m][n];
+        System.out.println();
+        System.out.println("Insira valores inteiros");
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
+                System.out.print("Linha "+(i+1)+" Coluna "+(j+1)+": ");
                 mat[i][j] = sc.nextInt();
-
             }
         }
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print(mat[i][j]+" | ");
+            }
+            System.out.println();
+        }
+        System.out.print("Informe um número da matriz: ");
+        int numero = sc.nextInt();
 
 
         sc.close();
