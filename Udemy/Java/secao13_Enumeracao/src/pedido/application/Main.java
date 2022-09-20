75bcf3a505270b0ef3b70d3b192e355dceceb869
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-20 14:51:38 -0300

    Enum OrderStatus created

---
+++ Udemy/Java/secao13_Enumeracao/src/pedido/application/Main.java
@@ -0,0 +1,7 @@
+package pedido.application;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Udemy/Java/secao13_Enumeracao/src/pedido/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package pedido.entities.enums;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+
+}
