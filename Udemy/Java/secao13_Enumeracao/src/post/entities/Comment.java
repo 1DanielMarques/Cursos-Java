bf73c94bec6c3a96807c426f7ff615a384c6f73b
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-26 14:51:43 -0300

    entidade Comment

--- Udemy/Java/secao13_Enumeracao/src/post/entities/Comment.java
+++ Udemy/Java/secao13_Enumeracao/src/post/entities/Comment.java
@@ -1,4 +1,18 @@
 package post.entities;
 
 public class Comment {
+    private String text;
+
+    public Comment() {
+
+    }
+    public Comment(String text) {
+        this.text = text;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
 }
