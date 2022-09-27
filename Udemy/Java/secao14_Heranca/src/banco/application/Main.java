dfc8c673db7db2590e3b572510c51e29b6dbfffa
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-27 17:08:40 -0300

    override e super

--- Udemy/Java/secao14_Heranca/src/banco/application/Main.java
+++ Udemy/Java/secao14_Heranca/src/banco/application/Main.java
@@ -7,23 +7,23 @@ import banco.entities.SavingsAccount;
 public class Main {
     public static void main(String[] args) {
 
-        Account acc = new Account(1001,"Alex",0.0);
+        Account acc = new Account(1001,"Alex",1000.0);
         BusinessAccount bcc = new BusinessAccount(1002,"Maria",0.0,500.0);
 
         //UPCASTING
         Account acc1 = bcc;
-        Account acc2 = new BusinessAccount(1003,"Bob",0.0,200.0);
-        Account acc3 = new SavingsAccount(1004,"Anna",0.0,0.01);
+        Account acc2 = new BusinessAccount(1003,"Bob",1000.0,500.0);
+        Account acc3 = new SavingsAccount(1004,"Anna",1000.0,0.01);
 
         //DOWNCASTING
         //BusinessAccount acc4 = acc2; Da erro
-        BusinessAccount acc4 = (BusinessAccount)acc2;
-        acc4.loan(100.0);
+        /* BusinessAccount acc4 = (BusinessAccount)acc2;
+        acc4.loan(100.0); */
 
         /* BusinessAccount acc5 = (BusinessAccount) acc3;
          Da erro pq acc3 nao eh business account e sim SavingsAccount */
 
-        if(acc3 instanceof BusinessAccount){
+        /* if(acc3 instanceof BusinessAccount){
             BusinessAccount acc5 = (BusinessAccount) acc3;
             acc5.loan(200.0);
             System.out.println("loan");
@@ -32,7 +32,17 @@ public class Main {
             SavingsAccount acc5 = (SavingsAccount) acc3;
             acc5.updateBalance();
             System.out.println("update");
-        }
+        } */
+        //------------------------------------------//
+        acc.withdraw(200.0);
+        System.out.println(acc.getBalance());
+
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
+
+        acc2.withdraw(200.0);
+        System.out.println(acc2.getBalance());
+
 
 
 
