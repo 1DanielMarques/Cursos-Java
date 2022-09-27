1a3a45a2cad8e1919ac9d285640a60c84d2dfdc0
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-27 16:52:32 -0300

    downcasting e instanceof

--- Udemy/Java/secao14_Heranca/src/banco/application/Main.java
+++ Udemy/Java/secao14_Heranca/src/banco/application/Main.java
@@ -15,6 +15,24 @@ public class Main {
         Account acc2 = new BusinessAccount(1003,"Bob",0.0,200.0);
         Account acc3 = new SavingsAccount(1004,"Anna",0.0,0.01);
 
+        //DOWNCASTING
+        //BusinessAccount acc4 = acc2; Da erro
+        BusinessAccount acc4 = (BusinessAccount)acc2;
+        acc4.loan(100.0);
+
+        /* BusinessAccount acc5 = (BusinessAccount) acc3;
+         Da erro pq acc3 nao eh business account e sim SavingsAccount */
+
+        if(acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("loan");
+        }
+        if(acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("update");
+        }
 
 
 
