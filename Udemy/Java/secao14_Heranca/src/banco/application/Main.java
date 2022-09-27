f9c32a123033a545c54b55a78fc76ae64c5f4226
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-27 16:34:04 -0300

    implementando BusinessAccount extends Account

--- Udemy/Java/secao14_Heranca/src/banco/application/Main.java
+++ Udemy/Java/secao14_Heranca/src/banco/application/Main.java
@@ -1,4 +1,11 @@
 package banco.application;
 
+import banco.entities.BusinessAccount;
+
 public class Main {
+    public static void main(String[] args) {
+        BusinessAccount account = new BusinessAccount();
+
+
+    }
 }
