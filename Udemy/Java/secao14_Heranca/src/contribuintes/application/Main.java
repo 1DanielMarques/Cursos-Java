d0db3d39c9f836bef2578a57f7583f2dd8fbdd31
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-29 17:08:45 -0300

    classe abstrata pessoa

--- Udemy/Java/secao14_Heranca/src/contribuintes/application/Main.java
+++ Udemy/Java/secao14_Heranca/src/contribuintes/application/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        System.out.print("Quantos contribuintes? ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
 
-
-
+        }
 
 
         sc.close();
---
+++ Udemy/Java/secao14_Heranca/src/contribuintes/entities/Pessoa.java
@@ -0,0 +1,22 @@
+package contribuintes.entities;
+
+public abstract class Pessoa {
+    private String nome;
+    private Double rendaAnual;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getRendaAnual() {
+        return rendaAnual;
+    }
+
+    public void setRendaAnual(Double rendaAnual) {
+        this.rendaAnual = rendaAnual;
+    }
+}
