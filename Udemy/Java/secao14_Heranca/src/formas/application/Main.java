490583ba71ee276656b20a052bab0e84934b8a86
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-29 16:58:22 -0300

    menu principal

--- Udemy/Java/secao14_Heranca/src/formas/application/Main.java
+++ Udemy/Java/secao14_Heranca/src/formas/application/Main.java
@@ -1,7 +1,41 @@
 package formas.application;
 
+import formas.entities.Circle;
+import formas.entities.Rectangle;
+import formas.entities.Shape;
+import formas.entities.enums.Color;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        List<Shape> list = new ArrayList<>();
+        System.out.print("Enter the number of shapes: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Shape #" + i + " data:");
+            System.out.print("Rectangle or Circle (r/c)?");
+            char ch = sc.next().charAt(0);
+            System.out.print("Color (BLACK/BLUE/RED): ");
+            Color color = Color.valueOf(sc.next());
+            if (ch == 'r') {
+                System.out.print("Width: ");
+                double width = sc.nextDouble();
+                System.out.print("Height: ");
+                double height = sc.nextDouble();
+                list.add(new Rectangle(color, width, height));
+            } else if (ch == 'c') {
+                System.out.print("Radius: ");
+                double radius = sc.nextDouble();
+                list.add(new Circle(color, radius));
+            }
+
+        }
+
+        sc.close();
+
 
     }
 }
