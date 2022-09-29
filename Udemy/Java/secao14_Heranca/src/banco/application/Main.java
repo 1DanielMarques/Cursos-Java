c30160c9a69024812d64b96231358a134c7d7fd5
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-29 14:38:53 -0300

    classe account abstract

--- Udemy/Java/secao14_Heranca/src/banco/application/Main.java
+++ Udemy/Java/secao14_Heranca/src/banco/application/Main.java
@@ -7,7 +7,7 @@ import banco.entities.SavingsAccount;
 public class Main {
     public static void main(String[] args) {
 
-        Account acc = new Account(1001, "Alex", 1000.0);
+     /*   Account acc = new Account(1001, "Alex", 1000.0);
         BusinessAccount bcc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
         //UPCASTING
@@ -17,13 +17,13 @@ public class Main {
 
         //DOWNCASTING
         //BusinessAccount acc4 = acc2; Da erro
-        /* BusinessAccount acc4 = (BusinessAccount)acc2;
-        acc4.loan(100.0); */
+         BusinessAccount acc4 = (BusinessAccount)acc2;
+        acc4.loan(100.0);
 
-        /* BusinessAccount acc5 = (BusinessAccount) acc3;
-         Da erro pq acc3 nao eh business account e sim SavingsAccount */
+         BusinessAccount acc5 = (BusinessAccount) acc3;
+         Da erro pq acc3 nao eh business account e sim SavingsAccount
 
-        /* if(acc3 instanceof BusinessAccount){
+         if(acc3 instanceof BusinessAccount){
             BusinessAccount acc5 = (BusinessAccount) acc3;
             acc5.loan(200.0);
             System.out.println("loan");
@@ -32,7 +32,7 @@ public class Main {
             SavingsAccount acc5 = (SavingsAccount) acc3;
             acc5.updateBalance();
             System.out.println("update");
-        } */
+        }
         //------------------------------------------//
         acc.withdraw(200.0);
         System.out.println(acc.getBalance());
@@ -49,8 +49,14 @@ public class Main {
         x.withdraw(50.0);
         y.withdraw(50.0);
         System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
+        System.out.println(y.getBalance()); */
+
+        //Account acc1 = new Account(1001, "Alex", 1000.0);
+        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
 
 
     }
 }
+
+
