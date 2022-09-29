cb712a70b38d86bc3ef91f65c64dd6c3528c81fb
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-29 17:12:13 -0300

    classe PessoaJuridica

---
+++ Udemy/Java/secao14_Heranca/src/contribuintes/entities/PessoaJuridica.java
@@ -0,0 +1,22 @@
+package contribuintes.entities;
+
+public class PessoaJuridica extends Pessoa {
+    private int funcionarios;
+
+    public PessoaJuridica() {
+        super();
+    }
+
+    public PessoaJuridica(String nome, Double rendaAnual, int funcionarios) {
+        super(nome, rendaAnual);
+        this.funcionarios = funcionarios;
+    }
+
+    public int getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(int funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+}
