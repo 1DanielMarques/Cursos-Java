34675f9b59412c4ff420b83fcd3102a9624a2768
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-29 17:41:50 -0300

    exercicio finalizado

--- Udemy/Java/secao14_Heranca/src/contribuintes/application/Main.java
+++ Udemy/Java/secao14_Heranca/src/contribuintes/application/Main.java
@@ -17,7 +17,7 @@ public class Main {
         System.out.print("Quantos contribuintes? ");
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
-            System.out.println("Dados contribuint #" + i + ": ");
+            System.out.println("Dados contribuinte #" + i + ": ");
             System.out.print("Pessoa Fisica ou Juridica (f/j)? ");
             char tipo = sc.next().charAt(0);
             System.out.print("Nome: ");
@@ -26,7 +26,7 @@ public class Main {
             System.out.print("Renda anual: R$");
             double rendaAnual = sc.nextDouble();
             if (tipo == 'f') {
-                System.out.println("Gastos com saúde: R$");
+                System.out.print("Gastos com saúde: R$");
                 double gastosSaude = sc.nextDouble();
                 list.add(new PessoaFisica(nome, rendaAnual, gastosSaude));
             } else {
@@ -34,15 +34,16 @@ public class Main {
                 int funcionarios = sc.nextInt();
                 list.add(new PessoaJuridica(nome, rendaAnual, funcionarios));
             }
-            System.out.println();
-            System.out.println("Taxas pagas: ");
-            for(Pessoa p : list){
-                System.out.println(p.imposto());
-            }
-
 
         }
-
+        System.out.println();
+        double total = 0;
+        System.out.println("Taxas pagas: ");
+        for (Pessoa p : list) {
+            System.out.println(p.getNome() + ": " + " R$" + String.format("%.2f", p.imposto()));
+            total += p.imposto();
+        }
+        System.out.print("\nTotal Impostos: R$" + String.format("%.2f",total));
 
         sc.close();
     }
