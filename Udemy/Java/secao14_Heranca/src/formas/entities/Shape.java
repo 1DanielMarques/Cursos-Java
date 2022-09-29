7fcc2270108b705a45fcf1250ed87f053d54b249
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-29 16:49:18 -0300

    construtores classe shape

--- Udemy/Java/secao14_Heranca/src/formas/entities/Shape.java
+++ Udemy/Java/secao14_Heranca/src/formas/entities/Shape.java
@@ -5,6 +5,14 @@ import formas.entities.enums.Color;
 public abstract class Shape {
     private Color color;
 
+    public Shape() {
+
+    }
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
     public Color getColor() {
         return color;
     }
