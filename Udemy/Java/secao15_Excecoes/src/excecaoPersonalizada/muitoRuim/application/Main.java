33abe097322bf9868e16ee60a9fb263c0ff843bc
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-30 14:52:52 -0300

    data atualizada

--- Udemy/Java/secao15_Excecoes/src/excecaoPersonalizada/muitoRuim/application/Main.java
+++ Udemy/Java/secao15_Excecoes/src/excecaoPersonalizada/muitoRuim/application/Main.java
@@ -17,14 +17,21 @@ public class Main {
         Date checkIn = sdf.parse(sc.next());
         System.out.print("Check-out date (dd/MM/yyyy): ");
         Date checkOut = sdf.parse(sc.next());
-        if(!checkOut.after(checkIn)){
+        if (!checkOut.after(checkIn)) {
             System.out.println("Error in reservation: Check-out date must be after check-in date");
-        }else{
+        } else {
             Reservation reservation = new Reservation(number, checkIn, checkOut);
             System.out.println("Reservation: " + reservation);
+            System.out.println();
+            System.out.println("Enter data to update the reservation: ");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = sdf.parse(sc.next());
+
+            reservation.updatedates(checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
         }
-
-
 
 
         sc.close();
