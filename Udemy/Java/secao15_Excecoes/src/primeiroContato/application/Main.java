9b4ea117197c05d6b66d6fd217fdb33ea023f926
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-30 11:17:42 -0300

    primeiro commit

---
+++ Udemy/Java/secao15_Excecoes/src/primeiroContato/application/Main.java
@@ -0,0 +1,13 @@
+package primeiroContato.application;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] vect = sc.nextLine().split(" ");
+        int position = sc.nextInt();
+        System.out.println(vect[position]);
+        sc.close();
+    }
+}
