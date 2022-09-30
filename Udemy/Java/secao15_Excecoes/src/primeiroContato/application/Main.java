c14483327b1f890f8430cfcfec92adf86ab4155a
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-30 11:21:34 -0300

    Estrutura try-catch

--- Udemy/Java/secao15_Excecoes/src/primeiroContato/application/Main.java
+++ Udemy/Java/secao15_Excecoes/src/primeiroContato/application/Main.java
@@ -1,13 +1,22 @@
 package primeiroContato.application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] vect = sc.nextLine().split(" ");
-        int position = sc.nextInt();
-        System.out.println(vect[position]);
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+        } catch (InputMismatchException e){
+            System.out.println("Input error!");
+        }
+
+
         sc.close();
     }
 }
