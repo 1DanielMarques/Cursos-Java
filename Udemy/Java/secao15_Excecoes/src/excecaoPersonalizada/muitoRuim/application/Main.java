b25e4253fecd3d1220a403c52e18f28ae9f933e7
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-30 14:59:37 -0300

    solucao muitoRuim

--- Udemy/Java/secao15_Excecoes/src/excecaoPersonalizada/muitoRuim/application/Main.java
+++ Udemy/Java/secao15_Excecoes/src/excecaoPersonalizada/muitoRuim/application/Main.java
@@ -29,8 +29,15 @@ public class Main {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
-            reservation.updatedates(checkIn, checkOut);
-            System.out.println("Reservation: " + reservation);
+            Date now = new Date();
+            if (checkIn.before(now) || checkOut.before(now)) {
+                System.out.println("Error in reservation: Reservation dates for update must be future dates");
+            } else if (!checkOut.after(checkIn)) {
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            }else{
+                reservation.updatedates(checkIn, checkOut);
+                System.out.println("Reservation: " + reservation);
+            }
         }
 
 
