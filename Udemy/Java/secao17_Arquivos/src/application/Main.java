554bc1cde003d19564c730d60d128830536cd6cb
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-11 14:31:44 -0300

    in.txt

---
+++ Udemy/Java/secao17_Arquivos/src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\nohax\\OneDrive\\Área de Trabalho\\Software\\REPOSITORIOS\\CursosJava\\Udemy\\Java\\secao17_Arquivos\\in.txt");
+        Scanner sc = null;
+
+
+
+    }
+}
