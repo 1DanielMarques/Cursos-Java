c911957d33e03240ddf003ff6c549b070ad1a317
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-11 14:52:24 -0300

    BufferedWriter

---
+++ Udemy/Java/secao17_Arquivos/src/application/Main4.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main4 {
+    public static void main(String[] args) {
+
+        String[] lines = new String[]{"Goord morning", "Good afternoon", "Good night"};
+
+        String path = "C:\\Users\\nohax\\OneDrive\\Área de Trabalho\\Software\\REPOSITORIOS\\CursosJava\\Udemy\\Java\\secao17_Arquivos\\out.txt";
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))){
+            //FileWriter(path) -> cria/recria o arquivo
+            //FileWriter(path,true) -> acrescenta no arquivo existente
+            for(String line : lines){
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
