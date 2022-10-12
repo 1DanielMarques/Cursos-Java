0fce1728e40c50e48c5dd977c765aee8b6316c6f
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-12 11:41:14 -0300

    logica leitura dados

--- Udemy/Java/secao17_Arquivos/src/exercicio/application/Main.java
+++ Udemy/Java/secao17_Arquivos/src/exercicio/application/Main.java
@@ -1,7 +1,35 @@
 package exercicio.application;
 
+import exercicio.entities.Produto;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String[] dados = null;
+        List<Produto> produtos = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\nohax\\OneDrive\\Área de Trabalho\\Software\\REPOSITORIOS\\CursosJava\\Udemy\\Java\\secao17_Arquivos\\exercicioProp\\in.txt"))) {
+            String line = br.readLine();
+            while (line != null) {
+                dados = line.split(",");
+                Produto p = new Produto(dados[0], Double.parseDouble(dados[1]), Integer.parseInt(dados[2]));
+                produtos.add(p);
+                line = br.readLine();
+            }
+            for (Produto p : produtos) {
+                System.out.println(p.getNome() + " | " + p.getPreco() + " | " + p.getQuantidade() + " | " + p.total());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
