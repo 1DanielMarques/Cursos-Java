89a278ab1cf375baae8e4d4b64216c949c0b8f56
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-14 15:13:51 -0300

    menu entrada dados

--- Udemy/Java/secao18_Interfaces/src/contrato/application/Main.java
+++ Udemy/Java/secao18_Interfaces/src/contrato/application/Main.java
@@ -1,9 +1,28 @@
 package contrato.application;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre os dados do contrato:");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDateTime date = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.print("Valor do contrato: R$");
+        double valor = sc.nextDouble();
+        System.out.print("Entre com o numero de parcelas: ");
+        int installment = sc.nextInt();
+        System.out.println("Parcelas: ");
 
 
-
+        sc.close();
     }
 }
---
+++ Udemy/Java/secao18_Interfaces/src/contrato/model/services/ContractService.java
@@ -0,0 +1,7 @@
+package contrato.model.services;
+
+import contrato.model.entities.Contract;
+
+public class ContractService {
+
+}
