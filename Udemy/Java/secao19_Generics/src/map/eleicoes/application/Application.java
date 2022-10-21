fc636c3f957fdfa57c77054c78adc05c7801ff9d
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-21 11:34:25 -0300

    exercicio eleicao finalizado

---
+++ Udemy/Java/secao19_Generics/src/map/eleicoes/application/Application.java
@@ -0,0 +1,42 @@
+package map.eleicoes.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Application {
+    public static void main(String[] args) {
+
+        final String PATH = "C:\\Users\\nohax\\OneDrive\\Área de Trabalho\\Software\\REPOSITORIOS\\CursosJava\\Udemy\\Java\\secao19_Generics\\src\\map\\eleicoes\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(PATH))) {
+            Map<String, Integer> votos = new HashMap<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] dados = line.split(",");
+                String name = dados[0];
+                int votoAtual = Integer.parseInt(dados[1]);
+
+                if (votos.containsKey(name)) {
+                    int votoAnterior = votos.get(name);
+                    votos.put(name, votoAtual + votoAnterior);
+                } else {
+                    votos.put(name, votoAtual);
+                }
+                line = br.readLine();
+            }
+            for (String key : votos.keySet()) {
+                System.out.println(key + ": " + votos.get(key));
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
