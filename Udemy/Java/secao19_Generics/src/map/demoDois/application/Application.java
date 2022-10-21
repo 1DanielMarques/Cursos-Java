415828d709dce56d60c7f7ef2d3872e586765e74
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-21 11:07:32 -0300

    demo um e dois

---
+++ Udemy/Java/secao19_Generics/src/map/demoDois/application/Application.java
@@ -0,0 +1,28 @@
+package map.demoDois.application;
+
+
+import map.demoDois.model.entities.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Map<Product, Double> stock = new HashMap<>();
+
+        Product p1 = new Product("Tv", 900.0);
+        Product p2 = new Product("Tv", 900.0);
+        Product p3 = new Product("Tv", 900.0);
+
+        stock.put(p1, 10000.0);
+        stock.put(p2, 20000.0);
+        stock.put(p3, 15000.0);
+
+        Product ps = new Product("Tv", 900.0);
+
+        System.out.println("Contais 'ps' key: " + stock.containsKey(ps));
+
+
+    }
+}
