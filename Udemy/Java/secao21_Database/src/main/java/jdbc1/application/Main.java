48ebe43c3ce7f80e1297000919f080810f243bb8
daniel <daniel.marques@edu.unifil.br>
2022-10-29 17:01:37 -0300

    SELECT * FROM department

--- Udemy/Java/secao21_Database/src/main/java/jdbc1/application/Main.java
+++ Udemy/Java/secao21_Database/src/main/java/jdbc1/application/Main.java
@@ -3,11 +3,26 @@ package jdbc1.application;
 import jdbc1.db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Connection con = DB.getConnection();
-        DB.closeConnection();
+        Connection con;
+        Statement stm;
+        ResultSet rs;
+        try {
+            con = DB.getConnection();
+            stm = con.createStatement();
+            rs = stm.executeQuery("SELECT * FROM department");
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
