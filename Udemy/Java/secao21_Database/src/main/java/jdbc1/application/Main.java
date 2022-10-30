a524e022aba97855f8cb7973bd4bdcce76bf4722
daniel <daniel.marques@edu.unifil.br>
2022-10-30 15:58:31 -0300

    sql statement prepared

--- Udemy/Java/secao21_Database/src/main/java/jdbc1/application/Main.java
+++ Udemy/Java/secao21_Database/src/main/java/jdbc1/application/Main.java
@@ -1,30 +1,39 @@
 package jdbc1.application;
 
 import jdbc1.db.DB;
+import jdbc1.db.DbException;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection con = null;
-        Statement stm = null;
-        ResultSet rs = null;
+        PreparedStatement pstm = null;
         try {
             con = DB.getConnection();
-            stm = con.createStatement();
-            rs = stm.executeQuery("SELECT * FROM department");
-            while (rs.next()) {
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-            }
+            pstm = con.prepareStatement(
+                    "INSERT INTO seller " +
+                            " (Name, Email, BirthDate, BaseSalary, DepartmentId) " +
+                            " VALUES " +
+                            " (?,?,?,?,?)");
+            pstm.setString(1, "Daniel Marques");
+            pstm.setString(2, "daniel@gmail.com");
+            pstm.setDate(3, new Date(sdf.parse("09/09/2003").getTime()));
+            pstm.setDouble(4, 3000.0);
+            pstm.setInt(5, 4);
+
         } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            DB.closeStatement(stm);
-            DB.closeResultSet(rs);
-            DB.closeConnection(con);
+            throw new DbException(e.getMessage());
+        } catch (ParseException e) {
+            throw new DbException(e.getMessage());
         }
+
+
     }
 }
