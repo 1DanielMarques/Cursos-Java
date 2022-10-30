4cef2dac8ac03b7179f4734063f5ba898f9ea1d2
daniel <daniel.marques@edu.unifil.br>
2022-10-30 16:09:58 -0300

    returning the id created

--- Udemy/Java/secao21_Database/src/main/java/jdbc1/application/Main.java
+++ Udemy/Java/secao21_Database/src/main/java/jdbc1/application/Main.java
@@ -2,10 +2,7 @@ package jdbc1.application;
 
 import jdbc1.db.DB;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -20,7 +17,8 @@ public class Main {
                     "INSERT INTO seller " +
                             " (Name, Email, BirthDate, BaseSalary, DepartmentId) " +
                             " VALUES " +
-                            " (?,?,?,?,?)");
+                            " (?,?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
             pstm.setString(1, "Daniel Marques");
             pstm.setString(2, "daniel@gmail.com");
             pstm.setDate(3, new Date(sdf.parse("09/09/2003").getTime()));
@@ -28,7 +26,17 @@ public class Main {
             pstm.setInt(5, 4);
             //pstm.execute();
             int rowsAffected = pstm.executeUpdate();
-            System.out.printf("Done! Rows affected: " + rowsAffected);
+            if (rowsAffected > 0) {
+                ResultSet rs = pstm.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Id = " + id);
+                }
+
+            } else {
+                System.out.printf("No rows affected!");
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ParseException e) {
