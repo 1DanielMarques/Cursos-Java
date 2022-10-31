a2b55e3a210f93749622fbffeaf9e69f9c3db1a4
daniel <daniel.marques@edu.unifil.br>
2022-10-31 10:20:19 -0300

    Update

--- Udemy/Java/secao21_Database/src/main/java/jdbc1/application/Main.java
+++ Udemy/Java/secao21_Database/src/main/java/jdbc1/application/Main.java
@@ -8,41 +8,25 @@ import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
         Connection con = null;
-        PreparedStatement pstm = null;
+        PreparedStatement st = null;
         try {
             con = DB.getConnection();
-            pstm = con.prepareStatement(
-                    "INSERT INTO seller " +
-                            " (Name, Email, BirthDate, BaseSalary, DepartmentId) " +
-                            " VALUES " +
-                            " (?,?,?,?,?)",
-                    Statement.RETURN_GENERATED_KEYS);
-            pstm.setString(1, "Daniel Marques");
-            pstm.setString(2, "daniel@gmail.com");
-            pstm.setDate(3, new Date(sdf.parse("09/09/2003").getTime()));
-            pstm.setDouble(4, 3000.0);
-            pstm.setInt(5, 4);
-            //pstm.execute();
-            int rowsAffected = pstm.executeUpdate();
-            if (rowsAffected > 0) {
-                ResultSet rs = pstm.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Id = " + id);
-                }
-
-            } else {
-                System.out.printf("No rows affected!");
-            }
-
+            st = con.prepareStatement(
+                    " UPDATE seller " +
+                            " SET BaseSalary =  BaseSalary + ? " +
+                            " WHERE " +
+                            " (DepartmentId = ?)");
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
+            //int rowsAffected = st.executeUpdate();
+            //System.out.printf("Rows Affected: " + rowsAffected);
+            st.execute();
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
         } finally {
-            DB.closeStatement(pstm);
+            DB.closeStatement(st);
             DB.closeConnection(con);
         }
 
