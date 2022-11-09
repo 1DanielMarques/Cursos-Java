002d44e17a71f18e6e6c552af383121cd10e13b8
daniel <daniel.marques@edu.unifil.br>
2022-11-09 14:51:27 -0300

    Connecting to mongoDB with repository and service

--- Udemy/Java/secao24_APISpring/workshopmongo/src/main/java/com/javacourse/workshopmongo/domain/User.java
+++ Udemy/Java/secao24_APISpring/workshopmongo/src/main/java/com/javacourse/workshopmongo/domain/User.java
@@ -1,12 +1,18 @@
 package com.javacourse.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+//@Document(collection = "user")
+@Document
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String name;
     private String email;
