deae2f29fcc79ff6bca384c5681dfdcfbfb7b6f4
daniel <daniel.marques@edu.unifil.br>
2022-11-10 15:23:49 -0300

    Getting Post by id

---
+++ Udemy/Java/secao24_APISpringMongo/workshopmongo/src/main/java/com/javacourse/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.javacourse.workshopmongo.services;
+
+import com.javacourse.workshopmongo.domain.Post;
+import com.javacourse.workshopmongo.repositories.PostRepository;
+import com.javacourse.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> Post = repository.findById(id);
+        return Post.orElseThrow(() -> new ObjectNotFoundException(id));
+    }
+
+
+}
