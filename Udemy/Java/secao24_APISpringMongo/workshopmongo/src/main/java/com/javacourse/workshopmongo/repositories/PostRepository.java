dcd01a018cc3eda0269d649e97031af930b936dd
daniel <daniel.marques@edu.unifil.br>
2022-11-10 14:46:54 -0300

    Creating Post with nested User

---
+++ Udemy/Java/secao24_APISpringMongo/workshopmongo/src/main/java/com/javacourse/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.javacourse.workshopmongo.repositories;
+
+import com.javacourse.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
